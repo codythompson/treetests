b0955e0cc2e5fbd9cc2541dd298b5444c06c4a12
codyThompson <cody.thompson22@gmail.com>
2016-08-30 18:56:29 -0700

    moved the templater to it's own file.

--- src/treetests.js
+++ src/treetests.js
@@ -31,64 +31,6 @@ var treetests = {
 *******************************************************************************/
 
 /*******************************************************************************
- * templater
-*******************************************************************************/
-var templater = {
-  remove_ids: true,
-
-  get_atts: function (ele) {
-    var atts = {
-      tt_atts: {},
-      atts: {}
-    };
-    for (var i = 0; i < ele.attributes.length; i++) {
-      var att = ele.attributes[i];
-      if (att.name.substr(0, treetests.tt_att_prefix.length) === treetests.tt_att_prefix) {
-        atts.tt_atts[att.name] = att.value;
-      } else if (!this.remove_ids || att.name !== 'id') {
-        atts.atts[att.name] = att.value;
-      }
-    }
-    return atts;
-  },
-
-  is_tt_tag: function (ele) {
-    return ele.tagName.toLowerCase().substr(0, treetests.tt_att_prefix.length) === treetests.tt_att_prefix;
-  },
-
-  templatize: function (ele) {
-    var dom_path = arguments[1] || [];
-
-    var template = {
-      dom_path: dom_path
-    };
-    if (ele.nodeType === 3) { // text node
-      template.type = 'text';
-      template.value = ele.data;
-    } else if (ele.nodeType === 1) { // html element
-      var atts = this.get_atts(ele);
-      template.type = (this.is_tt_tag(ele) && 'tt-ele') || 'ele';
-      template.tag = ele.tagName.toLowerCase();
-      template.tt_atts = atts.tt_atts;
-      template.atts = atts.atts;
-    } else {
-      console.warn('[treetests] Unable to templatize node of type ' + ele.nodeType + '\nDOM PATH: ' + JSON.stringify(dom_path));
-      return null;
-    }
-
-    template.children = [];
-    for (var i = 0; ele.childNodes && i < ele.childNodes.length; i++) {
-      var child = this.templatize(ele.childNodes[i], dom_path.concat([i]));
-      if (child) {
-        template.children.push(child);
-      }
-    }
-
-    return template;
-  }
-};
-
-/*******************************************************************************
  * dom_builder
 *******************************************************************************/
 var dom_builder = {
@@ -241,7 +183,6 @@ var dom_builder = {
 /*******************************************************************************
  * export
 *******************************************************************************/
-treetests.templater = templater;
 treetests.dom_builder = dom_builder;
 
 scope.treetests = treetests;
