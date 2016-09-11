8296a5e2d47c70079a44bb9a936c1d770babada1
codyThompson <cody.thompson22@gmail.com>
2016-09-11 09:34:44 -0700

    moved comment describing the test result format to test_suite.js

--- src/treetests.js
+++ src/treetests.js
@@ -1,22 +1,3 @@
-/*
- * test_data cat fields:
- * name // name/label of the category
- * type // string 'category'
- * cats_passed // number of child categories that passed all tests
- * cat_count // number of child categories
- * tests_passed // number of tests passed stricly within this cat
- * test_count // number of tests stricly within this cat
- * total_tests_passed // number of all descendant tests that passed
- * total_test_count // number of all descendant tests
- * children // child tests or categories
- *
- * test_data test fields:
- * name // name/label of the test
- * type // string 'test'
- * passed // boolean indicating whether on not the test passed
- * message // optional error message for test
- */
-
 (function (scope) {
 
 var treetests = {
